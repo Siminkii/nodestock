451158fae15b8eb2832e38d1e54e20c4e76cfdf0
Siminkii <saima.hrnciarova@gmail.com>
2024-12-10 11:54:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//set Handlebars Middleware
+app.engine('handlebars', engine({ extname: 'handlebars', defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const otherstuff = "hello there, this is other stuff!";
+
+// Set handlebar routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+
+ app.use(express.static(path.join(__dirname, 'public')));
+ app.listen(PORT, () => console.log('Server Listening on port' + PORT));
