8d9daead65502e9e7dcd3e82d06543fb403c0a3a
Siminkii <saima.hrnciarova@gmail.com>
2024-12-10 16:05:29 +0100

    created procfile for heroku

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+//API KEY fgW2Im6HchoZPBAT_pRq5NPooloAPY8b
+request('https://api.polygon.io/v2/last/trade/AAPL?apiKey=fgW2Im6HchoZPBAT_pRq5NPooloAPY8b', { json: true }, (err,res, body) => {
+    if (err) {return console.log(err);}
+    console.log(body);
+    if (res.statusCode === 200){
+        console.log(body);
+    };
+});
+
+
+
+
+
+
 
 //set Handlebars Middleware
 app.engine('handlebars', engine({ extname: 'handlebars', defaultLayout: "main"}));
@@ -20,7 +36,10 @@ app.get('/', (req, res) => {
         stuff: otherstuff
     });
 });
-
+ // Create about page route
+app.get('/about.html', (req, res) => {
+    res.render('about');
+});
 
  app.use(express.static(path.join(__dirname, 'public')));
  app.listen(PORT, () => console.log('Server Listening on port' + PORT));
